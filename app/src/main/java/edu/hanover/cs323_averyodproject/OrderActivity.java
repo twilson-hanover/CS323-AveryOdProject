3c1ab57dea2218d6bc8db7dc330f6654dc37b4a1
chezema21 <chezema21@hanover.edu>
2018-05-22 14:02:06 -0400

    Order Page

---
+++ app/src/main/java/edu/hanover/cs323_averyodproject/OrderActivity.java
@@ -0,0 +1,15 @@
+package edu.hanover.cs323_averyodproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class OrderActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+
+}
