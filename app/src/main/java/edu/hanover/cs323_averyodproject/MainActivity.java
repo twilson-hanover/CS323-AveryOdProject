6f0d618cfb153112aeb6ad502fcfb3066c963a36
ganzorigo21 <od.ganzorig12@gmail.com>
2018-05-23 11:53:12 -0400

    added main activity

--- app/src/main/java/edu/hanover/cs323_averyodproject/MainActivity.java
+++ app/src/main/java/edu/hanover/cs323_averyodproject/MainActivity.java
@@ -2,6 +2,9 @@ package edu.hanover.cs323_averyodproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Menu;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    //add items to the action bar
+    public boolean onCreateOptionsMenu(Menu menu){
+        //inflate the menu; this adds items to the action bar if it's present
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.action_create_order:
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
